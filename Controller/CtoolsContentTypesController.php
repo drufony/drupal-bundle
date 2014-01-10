ddf37aedd68335bee8205060cb08da0971c0bb10
Benjamin Doherty <bjd@bangpound.org>
2014-01-09 23:34:40 -0600

    create rudimentary ctools content type controller.
    This is far from useful, because ctools needs contexts to render most content types.

---
+++ Controller/CtoolsContentTypesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Bangpound\Bundle\DrupalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/**
+ * Class CtoolsContentTypesController
+ * @Route("/ctools")
+ * @package Bangpound\Bundle\DrupalBundle\Controller
+ */
+class CtoolsContentTypesController extends Controller
+{
+    public function __construct()
+    {
+        ctools_include('content');
+    }
+
+    /**
+     * @Route("/{type}/{subtype}", name="ctools_content_render")
+     * @Method({"GET", "POST"})
+     * @Template
+     * @param  string                  $type
+     * @param  string                  $subtype
+     * @param  array                   $conf
+     * @param  array                   $keywords
+     * @param  array                   $args
+     * @param  array                   $context
+     * @param  string                  $incoming_content
+     * @throws BadRequestHttpException
+     * @return array
+     */
+    public function renderAction($type, $subtype, $conf = [], $keywords = array(), $args = array(), $context = array(), $incoming_content = '')
+    {
+        $block = ctools_content_render($type, $subtype, $conf, $keywords, $args, $context, $incoming_content);
+        if ($block) {
+            return (array) $block;
+        } else {
+            throw new BadRequestHttpException(sprintf('Cannot render ctools content type %s, %s', $type, $subtype));
+        }
+    }
+}
