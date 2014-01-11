85cf499f4bb6e280cdfa78678d00899654c8c271
Benjamin Doherty <bjd@bangpound.org>
2014-01-10 19:04:08 -0600

    revert to stable pimple.

--- Bootstrap.php
+++ Bootstrap.php
@@ -22,28 +22,28 @@ class Bootstrap extends AutoloadBootstrap
          *
          * @see _drupal_bootstrap_configuration()
          */
-        $this[DRUPAL_BOOTSTRAP_CONFIGURATION] = function () {
-        };
+        $this[DRUPAL_BOOTSTRAP_CONFIGURATION] = $this->share(function () {
+        });
 
-        $this[DRUPAL_BOOTSTRAP_PAGE_CACHE] = function () {
+        $this[DRUPAL_BOOTSTRAP_PAGE_CACHE] = $this->share(function () {
             // Allow specifying special cache handlers in settings.php, like
             // using memcached or files for storing cache information.
             require_once DRUPAL_ROOT . '/includes/cache.inc';
             foreach (variable_get('cache_backends', array()) as $include) {
                 require_once DRUPAL_ROOT . '/' . $include;
             }
-        };
+        });
 
         // DRUPAL_BOOTSTRAP_DATABASE - in parent class.
         // DRUPAL_BOOTSTRAP_VARIABLES - in base class.
         // DRUPAL_BOOTSTRAP_SESSION - in base class.
 
-        $this[DRUPAL_BOOTSTRAP_PAGE_HEADER] = function () {
-        };
+        $this[DRUPAL_BOOTSTRAP_PAGE_HEADER] = $this->share(function () {
+        });
 
         // DRUPAL_BOOTSTRAP_LANGUAGE
 
-        $this[DRUPAL_BOOTSTRAP_FULL] = function () {
+        $this[DRUPAL_BOOTSTRAP_FULL] = $this->share(function () {
             require_once DRUPAL_ROOT . '/includes/common.inc';
             require_once DRUPAL_ROOT . '/' . variable_get('path_inc', 'includes/path.inc');
             require_once DRUPAL_ROOT . '/includes/theme.inc';
@@ -59,6 +59,6 @@ class Bootstrap extends AutoloadBootstrap
             require_once DRUPAL_ROOT . '/includes/ajax.inc';
             require_once DRUPAL_ROOT . '/includes/token.inc';
             require_once DRUPAL_ROOT . '/includes/errors.inc';
-        };
+        });
     }
 }
