e8a05141425ee0d0d4657f8b2bf936db96f1f6a0
Benjamin Doherty <bjd@bangpound.org>
2014-01-15 17:20:23 -0600

    merge up.

--- Bootstrap.php
+++ Bootstrap.php
@@ -21,6 +21,11 @@ class Bootstrap extends AutoloadBootstrap
      */
     public function __construct(array $values = array())
     {
+        // If this bootstrap object is used in a service, bootstrap.inc may
+        // not have been included yet. If the file is not included, the
+        // Drupal bootstrap constants are not available.
+        require_once $values['DRUPAL_ROOT'] .'/includes/bootstrap.inc';
+
         parent::__construct($values);
 
         /**
