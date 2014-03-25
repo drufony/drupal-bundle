a5e6c9da13830b1b0dfa78be85d2ceb10f8cb176
Benjamin Doherty <bjd@bangpound.org>
2014-03-25 15:11:32 -0500

    coding standards.

--- Command/DrushCommand.php
+++ Command/DrushCommand.php
@@ -8,9 +8,16 @@ use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
+/**
+ * Class DrushCommand
+ * @package Bangpound\Bundle\DrupalBundle\Command
+ */
 class DrushCommand extends ContainerAwareCommand
 {
 
+    /**
+     * {@inheritDocs}
+     */
     protected function configure()
     {
         $this
@@ -20,6 +27,9 @@ class DrushCommand extends ContainerAwareCommand
         ;
     }
 
+    /**
+     * {@inheritDocs}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $args = array_splice($_SERVER['argv'], 2);
